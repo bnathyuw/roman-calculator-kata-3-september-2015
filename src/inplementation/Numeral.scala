afea32bdd011a1bfad273632857c4837f3062301
matthew.butt@7digital.com <Matthew Butt>
2015-09-03 16:52:54 +0100

    Refactor: extract Seqs of rules and ordering function.

--- src/inplementation/Numeral.scala
+++ src/inplementation/Numeral.scala
@@ -1,19 +1,29 @@
 package inplementation
 
 case class Numeral(value: String) {
+  val denormalisations = Seq(
+    ("IV", "IIII"),
+    ("IX", "VIIII")
+  )
+  val normalisations = Seq(
+    ("IIIII", "V"),
+    ("IIII", "IV"),
+    ("VV", "X"),
+    ("VIV", "IX")
+  )
+
   def +(other: Numeral) = {
     val s = denormalise(this.value) + denormalise(other.value)
     Numeral(normalise(s))
   }
 
-  private def normalise(input: String): String = {
-    input.sortWith{
-      case ('I', _) => false
-      case _ => true
-    } replace("IIIII", "V") replace("IIII", "IV") replace("VV", "X") replace ("VIV", "IX")
+  private def denormalise(input: String): String = denormalisations.foldLeft(input)(doReplace)
+  private def normalise(input: String): String = normalisations.foldLeft(input sortWith romanOrder)(doReplace)
+
+  private def doReplace(input: String, rule: (String, String)) = {
+    input replace(rule._1, rule._2)
   }
 
-  private def denormalise(input: String): String = {
-    input replace("IV", "IIII") replace("IX", "VIIII")
-  }
+  private def romanOrder(a: Char, b: Char) = a != 'I'
+
 }
