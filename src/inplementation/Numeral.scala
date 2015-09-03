e444336e52fa8b0f5dc49f81bac082be8bc379f9
matthew.butt@7digital.com <Matthew Butt>
2015-09-03 16:13:13 +0100

    Some more test cases: decompose V in inputs.

--- src/inplementation/Numeral.scala
+++ src/inplementation/Numeral.scala
@@ -1,5 +1,19 @@
 package inplementation
 
 case class Numeral(value: String) {
-  def +(other: Numeral) = Numeral(this.value + other.value replace("IIIII", "V"))
+  def +(other: Numeral) = {
+    val s = denormalise(this.value) + denormalise(other.value)
+    Numeral(normalise(s))
+  }
+
+  private def normalise(input: String): String = {
+    input.sortWith{
+      case ('I', _) => false
+      case _ => true
+    } replace("IIIII", "V") replace("IIII", "IV") replace("VV", "X") replace ("VIV", "IX")
+  }
+
+  private def denormalise(input: String): String = {
+    input replace("IV", "IIII")
+  }
 }
--- src/tests/AddingSpec.scala
+++ src/tests/AddingSpec.scala
@@ -11,4 +11,20 @@ class AddingSpec extends FlatSpec with Matchers{
   "adding II and III" should "give V" in {
     Numeral("II") + Numeral("III") should be (Numeral("V"))
   }
+
+  "adding V and V" should "give X" in {
+    Numeral("V") + Numeral("V") should be (Numeral("X"))
+  }
+
+  "adding IV and VI" should "give X" in {
+    Numeral("IV") + Numeral("VI") should be (Numeral("X"))
+  }
+
+  "adding V and IV" should "give IX" in {
+    Numeral("V") + Numeral("IV") should be (Numeral("IX"))
+  }
+
+  "adding I and III" should "give IV" in {
+    Numeral("I") + Numeral("III") should be (Numeral("IV"))
+  }
 }
